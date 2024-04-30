4419a83e142576b28529232f4971a51684c4d26d
divyasshree-BQ <124589840+divyasshree-BQ@users.noreply.github.com>
2024-04-30 17:45:42 +0530

    Update complete_code.py

--- complete_code.py
+++ complete_code.py
@@ -9,78 +9,89 @@ def authenticate_gsheets():
              "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_name('google_sheets.json', scope)
     client = gspread.authorize(creds)
-    spreadsheet = client.create("test_bitquery_transfer")
+    spreadsheet = client.create("test_bitquery_DEXTrades")
     # Get the first worksheet
     worksheet = spreadsheet.sheet1
     spreadsheet.share('divyasshree@bitquery.io', perm_type='user', role='writer')
+    print(f"Spreadsheet URL: {spreadsheet.url}")  # Print URL for direct access
     return worksheet
 
-# Fetch data from Bitquery API
+# Fetch data from Bitquery API using OAuth
 def fetch_bitquery_data():
-    url = "https://graphql.bitquery.io/"
-    headers = {'Content-Type': 'application/json', 'X-API-KEY': 'keyy'}
+    url = "https://streaming.bitquery.io/graphql"
+    headers = {'Authorization': 'Bearer ory_at_eEbFJHmd64CBPm1cm58V3y0qjYm1LfFtTGiF6PkZEvA.47AuZYHst9XUD_ESAQBRhWBjew0YQRrZfLJmHoAJntQ', 'Content-Type': 'application/json'}
     query = """
     {
-        ethereum(network: ethereum) {
-            transfers(
-                options: {desc: "block.height", limit: 10, offset: 0}
-                time: {since: "2024-04-30T09:59:06.000Z", till: "2024-04-30T10:29:06.999Z"}
-                amount: {gt: 0}
-            ) {
-                block {
-                    timestamp {
-                        time(format: "%Y-%m-%d %H:%M:%S")
-                    }
-                    height
-                }
-                sender {
-                    address
-                    annotation
-                }
-                receiver {
-                    address
-                    annotation
-                }
-                currency {
-                    address
-                    symbol
-                }
-                amount
-                amount_usd: amount(in: USD)
-                transaction {
-                    hash
-                }
-                external
+      EVM(dataset: archive, network: eth) {
+        DEXTrades(limit: {count: 5}, orderBy: {descending: Block_Time}) {
+          Block {
+            Number
+            Time
+          }
+          Transaction {
+            From
+            To
+            Hash
+          }
+          Trade {
+            Buy {
+              Amount
+              Buyer
+              Currency {
+                Name
+                Symbol
+                SmartContract
+              }
+              Seller
+              Price
+              PriceInUSD
             }
+            Sell {
+              Amount
+              Buyer
+              Currency {
+                Name
+                SmartContract
+                Symbol
+              }
+              Seller
+              Price
+            }
+            Dex {
+              ProtocolFamily
+              ProtocolName
+              SmartContract
+              Pair {
+                SmartContract
+              }
+            }
+          }
         }
+      }
     }
     """
     response = requests.post(url, json={'query': query}, headers=headers)
     if response.status_code == 200:
-        # print(response.text)
         return json.loads(response.text)
     else:
         raise Exception("Query failed and return code is {}. {}".format(response.status_code, response.text))
 
 # Write data to Google Sheets
 def update_sheet(worksheet, data):
-    transfers = data['data']['ethereum']['transfers']
-    for i, transfer in enumerate(transfers, start=2):  # Assuming headers are in the first row
-        worksheet.update(
-            range_name=f'A{i}:D{i}', 
-            values=[[
-                transfer['currency']['symbol'],
-                transfer['amount'],
-                transfer['sender']['address'],
-                transfer['receiver']['address']
-            ]]
-        )
+    trades = data['data']['EVM']['DEXTrades']
+    for i, trade in enumerate(trades, start=2):  # Assuming headers are in the first row
+        values = [
+            trade['Block']['Time'],
+            trade['Transaction']['Hash'],
+            trade['Trade']['Buy']['Amount'],
+            trade['Trade']['Buy']['Currency']['Symbol'],
+            trade['Trade']['Sell']['Amount'],
+            trade['Trade']['Sell']['Currency']['Symbol'],
+            trade['Trade']['Dex']['ProtocolName']
+        ]
+        # Update the sheet using named arguments
+        worksheet.update(range_name=f'A{i}:G{i}', values=[values])  
 
-# # Read data from Google Sheets
-# def read_sheet(worksheet):
-#     # Read all values in the worksheet
-#     data = worksheet.get_all_values()
-#     return data
 
 def main():
     worksheet = authenticate_gsheets()
